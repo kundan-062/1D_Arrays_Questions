18f88d036f4fe584f868b5c56fec5a51efd38383
Kundan Kumar <111641891+kundan-062@users.noreply.github.com>
2023-09-26 00:58:05 +0530

    Ass_Q1.java

--- Ass_Q1.java
+++ Ass_Q1.java
@@ -1,1 +1,13 @@
-
+//WAP to print the sum of all the elements present on even indexes in the given array.
+public class q1 {
+    public static void main(String[] args) {
+        int arr[] = {3,20,4,6,9};
+        //int arr[] = {4,3,6,7,1}
+   	 int i = 0, sum = 0;
+     while (i < arr.length) {
+         sum += arr[i];
+         i += 2;
+     }
+     System.out.println(sum);
+    }
+}
